8522bab2787fadc044381a48d1ca3f7d4c605846
AntonLyndin <alyndin@mail.ru>
2022-04-07 08:54:27 +0300

    Added new example

---
+++ Input.py
@@ -0,0 +1,17 @@
+import time
+
+
+print('What is your name')
+x=input()
+print('What is the place')
+y=input()
+print('What time is it')
+z=input()
+print('Well, Hello ' + x + '.')
+time.sleep(1)
+print('You are in ' + y  + '.')
+time.sleep(1)
+print('Time is ' + z + '.')
+time.sleep(1)
+
+
